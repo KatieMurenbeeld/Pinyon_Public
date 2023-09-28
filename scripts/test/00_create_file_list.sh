89fce2178d7f4f35c088a72a86a1fcfb19035c58
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-09-28 13:54:44 -0600

    renamed script and started new script to create metadata

--- scripts/test/00_create_file_list.sh
+++ scripts/test/00_create_file_list.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-find data/original -name "*.pdf" | grep '/Decision/\|CE\|Categorical Exclusion\|DM\|Decision Memo' | grep -v 'Draft' > file_list.txt
+find data/original -name "*.pdf" > all_files.txt
 
+find data/original -name "*.pdf" | grep '/Decision/\|CE\|Categorical Exclusion\|DM\|Decision Memo' > decision_file_list.txt
+
+find data/original -name "*.pdf" | grep '/Decision/\|CE\|Categorical Exclusion\|DM\|Decision Memo' | grep -v 'Draft' > decision_file_list_nodraft.txt
+
+cut -d "/" -f 3,4- file_list.txt | tr -s '[/]' ',' > decision_file_list_for_metadata.txt
+
+
+
+https://usfs-public.app.box.com/v/PinyonPublic/folder/158016778405
